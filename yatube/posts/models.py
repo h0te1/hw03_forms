9d8fdf85d2efcbc53f46df22f28ecad5a55fb6ca
Никита Зайцев <genius.zaytsev@gmail.com>
2023-01-31 22:55:26 +0500

    финал

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -18,12 +18,12 @@ class Post(models.Model):
         blank=False,
         null=False,
         verbose_name='Текст'
-        )
+    )
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        verbose_name='Автор',
+        verbose_name='автор',
         related_name='posts',
     )
     group = models.ForeignKey(
